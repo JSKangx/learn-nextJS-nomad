24dd29de26f15406d93ec39edd3cdadff0742763
Jinsu <injnamek@gmail.com>
2025-03-12 15:59:09 +0900

    feat: 동적 메타 데이터 설정

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../style/movie-videos.module.css";
 
 // 무비 비디오를 가져오는 함수
 async function getVideos(id: string) {
@@ -9,5 +10,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
 }
